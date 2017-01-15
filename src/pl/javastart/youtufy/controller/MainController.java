4f4d4fa90a91d253b38ebb7ff92f3290fb8968ee
Wojciech Kopeć <wojciechkopec93@gmail.com>
2017-01-15 20:44:56 +0100

    FXML & Controller classes added

--- src/pl/javastart/youtufy/controller/MainController.java
+++ src/pl/javastart/youtufy/controller/MainController.java
@@ -1,44 +1,13 @@
 package pl.javastart.youtufy.controller;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.web.WebView;
-
-public class MainController {
-
-    @FXML
-    private Button nextButton;
-
-    @FXML
-    private Slider volumeSlider;
-
-    @FXML
-    private Button previousButton;
-
-    @FXML
-    private MenuBar mainMenu;
-
-    @FXML
-    private Slider mediaSlider;
-
-    @FXML
-    private TableView<?> resultTableView;
-
-    @FXML
-    private WebView videoWebView;
-
-    @FXML
-    private TextField searchTextField;
-
-    @FXML
-    private ListView<?> historyListView;
-
-    @FXML
-    private Button playButton;
-
+ 
+import java.net.URL;
+import java.util.ResourceBundle;
+ 
+import javafx.fxml.Initializable;
+ 
+public class MainController implements Initializable {
+ 
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+    }
 }
--- src/pl/javastart/youtufy/main/Main.java
+++ src/pl/javastart/youtufy/main/Main.java
@@ -1,24 +1,22 @@
 package pl.javastart.youtufy.main;
- 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
- 
+
 public class Main extends Application {
- 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource(
-                "/pl/javastart/youtufy/view/MainPane.fxml"));
-        Scene scene = new Scene(parent);
-        stage.setScene(scene);
-        stage.setTitle("Youtufy v0.1");
-        stage.show();
-    }
- 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent parent = FXMLLoader.load(getClass().getResource("/pl/javastart/youtufy/view/MainPane.fxml"));
+		Scene scene = new Scene(parent);
+		stage.setScene(scene);
+		stage.setTitle("Youtufy v0.5");
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
